b47092b7207d850445ec8a8c0b8be70db8cc5e57
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-07-28 19:17:22 +0200

    3.5. kalkulator

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,14 +1,23 @@
 package com.kodilla.basic_assertion;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Calculator {
 
     public int sum(int a, int b) {
         return a + b;
     }
+
     public int subtract(int a, int b) {
         return a - b;
     }
-    public int pow(int c, int d) {
-        return c^d;
+
+    static class MathFunctions {
+        public static void main(String[] args) {
+            double first = 3.0;
+            int second = 2;
+            double sqrt = Math.pow(first, second);
+        }
     }
 }
+
