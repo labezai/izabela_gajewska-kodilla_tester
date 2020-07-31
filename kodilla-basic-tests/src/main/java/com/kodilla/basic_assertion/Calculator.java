31f7785b3aa321a0cb07a69fb6f203a58336e9e8
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-07-31 08:28:03 +0200

    3.6 testy jednostkowe kalkulator

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,7 +1,5 @@
 package com.kodilla.basic_assertion;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class Calculator {
 
     public int sum(int a, int b) {
@@ -12,12 +10,9 @@ public class Calculator {
         return a - b;
     }
 
-    static class MathFunctions {
-        public static void main(String[] args) {
-            double first = 3.0;
-            int second = 2;
-            double sqrt = Math.pow(first, second);
-        }
+    public static double mathFunctions(double first, int second) {
+        double pow = Math.pow(first, second);
+        return pow;
     }
 }
 
---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+    Calculator calculator = new Calculator();
+    int a = 5;
+    int b = 8;
+
+    @Test
+    public void testSum() {
+        int sumResult = calculator.sum(a, b);
+        assertEquals(14, sumResult);
+    }
+
+    @Test
+    public void testSubtract() {
+        int substractResult = calculator.subtract(a, b);
+        assertEquals(-3, substractResult);
+    }
+
+    @Test
+    public void testMathFunctions() {
+        double first = 3;
+        int second = 2;
+        double powResult = Calculator.mathFunctions(first, second);
+        assertEquals(9, powResult);
+    }
+}
