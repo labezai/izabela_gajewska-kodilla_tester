69e8f621bec086af19635ff8c62a0f65e5723bd5
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-08-07 18:32:02 +0200

    2.5 cwiczenia petle

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -0,0 +1,32 @@
+//package com.kodilla.bank.homework;
+//
+//public class CashMachine {
+//    private Bank bilans;
+//    private Bank liczbaWplat;
+//    private Bank liczbaWyplat;
+//    private Bank sredniaWplat;
+//    private Bank sredniaWyplat;
+//
+//    public CashMachine() {
+//        this.bilans = new Bank();
+//        this.liczbaWplat = new Bank();
+//        this.liczbaWyplat = new Bank();
+//        this.sredniaWplat = new Bank();
+//        this.sredniaWyplat = new Bank();
+//    }
+//    public void addWplataBank(int bank) {
+//        if(bank > 0) {
+//            this.liczbaWplat.add(bank);
+//        }
+//    }
+//    public double addWyplataBank(int bank) {
+//        if(bank < 0) {
+//            this.liczbaWyplat.add(bank);
+//        }
+//        public double getLiczbaWplatAverage() {
+//        return this.liczbaWplat.getAverage();
+//    }
+//        public double getLiczbaWyplatAverage() {
+//            return this.liczbaWyplat.getAverage();
+//        }
+//}
--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,27 +1,27 @@
-import java.util.Random;
-
-public class RandomNumbers {
-    int min = 0;
-    int max = 30;
-
-    public static int getRandomInt(int min, int max) {
-        int x = 0;
-        Random x = new random();
-        x = random.nextInt(max - min + 1) + min;
-        return x;
-    }
-    public static void main(String[] args) {
-        int sum = 0;
-        int zmien = 0;
-        int maxim = 30;
-        while (sum < 5000) {
-            zmien = getRandomInt(0, 30);
-            sum += zmien;
-            if (maxim < zmien) maxim = zmien;
-            if (minim > zmien) minim = zmien;
-            sum = sum + zmien;
-        }
-        System.out.println("Największa liczba to" + maxim + ",");
-        System.out.println("Najmniejsza liczba to" + minim + ".");
-    }
-}
+//import java.util.Random;
+//
+//public class RandomNumbers {
+//    int min = 0;
+//    int max = 30;
+//
+//    public static int getRandomInt(int min, int max) {
+//        int x = 0;
+//        Random x = new random();
+//        x = random.nextInt(max - min + 1) + min;
+//        return x;
+//    }
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        int zmien = 0;
+//        int maxim = 30;
+//        while (sum < 5000) {
+//            zmien = getRandomInt(0, 30);
+//            sum += zmien;
+//            if (maxim < zmien) maxim = zmien;
+//            if (minim > zmien) minim = zmien;
+//            sum = sum + zmien;
+//        }
+//        System.out.println("Największa liczba to" + maxim + ",");
+//        System.out.println("Najmniejsza liczba to" + minim + ".");
+//    }
+//}
