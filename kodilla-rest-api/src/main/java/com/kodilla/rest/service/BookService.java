394b5b53175379e9aa56ac607f877fb3aa779cb2
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-09-07 00:30:05 +0200

    10.3 API

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/service/BookService.java
@@ -0,0 +1,25 @@
+package com.kodilla.rest.service;
+
+import com.kodilla.rest.domain.BookDto;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+    private List<BookDto> books = new ArrayList<>();
+
+    public List<BookDto> getBooks() {
+        return books;
+    }
+
+    public void addBook(BookDto bookDto) {
+        books.add(bookDto);
+    }
+
+    public void remove(Book book){
+
+    }
+}
