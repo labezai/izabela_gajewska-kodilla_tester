b198f843503174abf51f3a0a4a94143ed9ed5fd4
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-09-07 00:45:00 +0200

    10.3 API

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,31 +1,31 @@
-package com.kodilla.basic_assertion;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class CalculatorTestSuite {
-    Calculator calculator = new Calculator();
-    int a = 5;
-    int b = 8;
-
-    @Test
-    public void testSum() {
-        int sumResult = calculator.sum(a, b);
-        assertEquals(13, sumResult);
-    }
-
-    @Test
-    public void testSubtract() {
-        int substractResult = calculator.subtract(a, b);
-        assertEquals(-3, substractResult);
-    }
-
-    @Test
-    public void testMathFunctions() {
-        double first = 3;
-        int second = 3;
-        double powResult = Calculator.mathFunctions(first, second);
-        assertEquals(9, powResult);
-    }
-}
+//package com.kodilla.basic_assertion;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class CalculatorTestSuite {
+//    Calculator calculator = new Calculator();
+//    int a = 5;
+//    int b = 8;
+//
+//    @Test
+//    public void testSum() {
+//        int sumResult = calculator.sum(a, b);
+//        assertEquals(13, sumResult);
+//    }
+//
+//    @Test
+//    public void testSubtract() {
+//        int substractResult = calculator.subtract(a, b);
+//        assertEquals(-3, substractResult);
+//    }
+//
+//    @Test
+//    public void testMathFunctions() {
+//        double first = 3;
+//        int second = 3;
+//        double powResult = Calculator.mathFunctions(first, second);
+//        assertEquals(9, powResult);
+//    }
+//}
