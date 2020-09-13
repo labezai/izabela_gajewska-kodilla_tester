1b0dce36914b8d46bb9d8aefe85114147b8791f1
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-09-13 20:22:00 +0200

    Calculator fixed

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -4,32 +4,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
-    double a = 10;
-    double b = 2;
     private Display display;
 
     public Calculator(Display display) {
         this.display = display;
     }
 
-    public Calculator() {
-        double a = 10;
-        double b = 2;
-    }
-
-    public String display(double value) {
-        if (checkValue(value)) {
-            return this.display.display(value);
-        }
-        return null;
-    }
-
-    private boolean checkValue(double value) {
-        return checkValue(value);
-    }
-
     public double add(double a, double b) {
-        return (a + b);
+        double result = 0;
+        result = a + b;
+        display.display(result);
+        return result;
     }
 
     public double subtract(double a, double b) {
