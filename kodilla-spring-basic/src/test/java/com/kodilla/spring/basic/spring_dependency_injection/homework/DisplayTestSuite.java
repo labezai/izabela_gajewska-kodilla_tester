105125bc77653964fac2d209090e300d2044aa28
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-09-29 08:19:23 +0200

    kalkulator bean

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/DisplayTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/DisplayTestSuite.java
@@ -1,52 +1,48 @@
-//package com.kodilla.spring.basic.spring_dependency_injection.homework;
-//
-//import org.junit.Test;
-//
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//@SpringBootTest
-//public class DisplayTestSuite {
-//    double a = 10;
-//    double b = 2;
-//    private Calculator calculator;
-//
-//    @Test
-//    public void shouldReturnCorrectSubtractValue() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double subtract = bean.subtract(10,2);
-//        double subtractResult = calculator.subtract(a, b);
-//        assertEquals(8, subtractResult);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectAddValue() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double add = bean.add(10, 2);
-//        double addResult = calculator.add(a, b);
-//        assertEquals(12, addResult);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectMultiplyValue() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double multiply = bean.multiply(10, 2);
-//        double multiplyResult = calculator.multiply(a, b);
-//        assertEquals(20, multiplyResult);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectDivideValue() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double divide = bean.divide(10,2);
-//        double divideResult = calculator.divide(a, b);
-//        assertEquals(5, divideResult);
-//    }
-//}
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+public class DisplayTestSuite {
+    double a = 10;
+    double b = 2;
+    private Calculator calculator;
+
+    @Test
+    public void shouldReturnCorrectSubtractValue() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator calculator = context.getBean(Calculator.class);
+        double subtractResult = calculator.subtract(a, b);
+        assertEquals(8, subtractResult, 0.1);
+    }
+
+    @Test
+    public void shouldReturnCorrectAddValue() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator calculator = context.getBean(Calculator.class);
+        double addResult = calculator.add(a, b);
+        assertEquals(12, addResult, 0.1);
+    }
+
+    @Test
+    public void shouldReturnCorrectMultiplyValue() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator calculator = context.getBean(Calculator.class);
+        double multiplyResult = calculator.multiply(a, b);
+        assertEquals(20, multiplyResult, 0.1);
+    }
+
+    @Test
+    public void shouldReturnCorrectDivideValue() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        Calculator calculator = context.getBean(Calculator.class);
+        double divideResult = calculator.divide(a, b);
+        assertEquals(5, divideResult, 0.1);
+    }
+}
