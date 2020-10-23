954deab35af7c5b92740351cd40bf725af589f0e
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-10-23 15:12:52 +0200

    selenium 2

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,17 +1,15 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class AllegroTestingApp {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
 
@@ -19,14 +17,9 @@ public class AllegroTestingApp {
                 By.xpath("//div//div//select"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByIndex(3);
+        categoryCombo.submit();
 
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("/html/body/div[2]/div[8]/div/div[2]/div/div[2]/button[2]")).click();
-        Alert simpleAlert = driver.switchTo().alert();
-        simpleAlert.accept();
-
-
-        WebElement inputField = driver.findElement(By.xpath("//INPUT[@type='search']/self::INPUT"));
+        WebElement inputField = driver.findElement(By.xpath("//input[@type='search']"));
         inputField.sendKeys("mavic mini");
         inputField.submit();
     }
